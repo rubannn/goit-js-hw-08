dbdcfab3da6c2e5d8df7d5af6ca5ff66d2ebf39c
m.ruban <m.ruban@testportal.gov.ua>
2025-08-20 15:01:31 +0200

    update js

--- js/gallery.js
+++ js/gallery.js
@@ -65,3 +65,19 @@ const images = [
 ];
 
 const galeryList = document.querySelector('.gallery');
+
+
+const galleryItems = images.map(
+    image =>
+        `<li class="gallery-item">
+            <a class="gallery-link" href="${image.original}">
+            <img class="gallery-image"
+                src="${image.preview}"
+                data-source="${image.original}"
+                alt="${image.description}" />
+            </a>
+        </li>`
+);
+
+// Додаємо всі елементи за одну операцію
+galeryList.insertAdjacentHTML('beforeend', galleryItems.join(''));
